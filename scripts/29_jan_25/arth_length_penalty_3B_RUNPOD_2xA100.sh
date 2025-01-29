87d6f4978cc76119e8879b77dac279c610f32de1
RobertMcCarthy97 <robertmccarthy97@gmail.com>
2025-01-29 18:05:09 +0000

    set length penalty to 0.8

--- scripts/29_jan_25/arth_length_penalty_3B_RUNPOD_2xA100.sh
+++ scripts/29_jan_25/arth_length_penalty_3B_RUNPOD_2xA100.sh
@@ -1,13 +1,13 @@
 export N_GPUS=2
 export BASE_ACTOR=Qwen/Qwen2.5-3B
 export BASE_CRITIC=Qwen/Qwen2.5-3B
-export MICRO_BATCH_SIZE=8
+export MICRO_BATCH_SIZE=4
 export DATA_DIR=/workspace/TinyZero/data/arth_default
 export ROLLOUT_TP_SIZE=1
-export EXPERIMENT_NAME=arth-qwen3B-illegal-strings-lvl-2
+export EXPERIMENT_NAME=arth-qwen3B-length-penalty-0.8
 export VLLM_ATTENTION_BACKEND=XFORMERS
 export USE_OVERSEER=True
-export OVERSEER_TYPE=arth_illegal_strings_lvl_2
+export OVERSEER_TYPE=length_penalty
 export KL_COEF=0.001
 
 # huggingface-cli login --token 
