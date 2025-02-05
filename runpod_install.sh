2186b7d859509e8fdd36b4cdd699716819f9db7e
RobertMcCarthy97 <robertmccarthy97@gmail.com>
2025-02-05 17:38:17 +0000

    add vague script

--- runpod_install.sh
+++ runpod_install.sh
@@ -27,3 +27,5 @@ python ./examples/data_preprocess/arth_prompt_replace_lvl1_decompose.py --local_
 python ./examples/data_preprocess/arth_super_simple_prompt_decompose_example.py --local_dir data/arth_super_simple_prompt_decompose_example
 
 python ./examples/data_preprocess/arth_prompt_replace_lvl2_decompose_example.py --local_dir data/arth_prompt_replace_lvl2_decompose_example
+
+python ./examples/data_preprocess/arth_prompt_replace_vague_lvl1_decompose.py --local_dir data/arth_prompt_replace_vague_lvl1_decompose
