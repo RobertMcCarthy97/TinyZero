c3dd63885a88d8d5cfb94861cf4700bbbd2d361f
RobertMcCarthy97 <robertmccarthy97@gmail.com>
2025-02-14 15:56:29 +0000

    add requirements

--- scripts/13_feb_sft/sft_syco.py
+++ scripts/13_feb_sft/sft_syco.py
@@ -1,6 +1,7 @@
 # %%
 
-# pip install trl
+# pip install conda install pytorch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 pytorch-cuda=12.1 -c pytorch -c nvidia
+# pip install transformers trl==0.14.0 datasets wandb
 
 # %%
 
