5067e1ffcdda9fc24015a0e1a7eba230ced1538c
RobertMcCarthy97 <robertmccarthy97@gmail.com>
2025-02-26 13:45:35 +0000

    add coin flip overseer stuff

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -67,6 +67,8 @@ def _select_CoT_rm_score_fn(reward_type):
         return pronto_illegal_strings_lvl_1.compute_score
     elif reward_type == "pronto_illegal_strings_lvl_2":
         return pronto_illegal_strings_lvl_2.compute_score
+    elif reward_type == "coin_flip_illegal_strings_lvl_1_dense":
+        return coin_flip_illegal_strings_lvl_1_dense.compute_score
     else:
         raise NotImplementedError
 
